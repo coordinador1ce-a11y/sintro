c0d2f22e7a74ef8f0e85a7695b0c9b92d886a90d
coordinador1ce-a11y <coordinador1.ce@gmail.com>
2026-08-14 20:38:02 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // ============================================================
 
 var CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxF7ntGOXPufT6nV9XSH7WJT6uTnIn8R5402K6b1B3uA6unQ2RgrcQtOgiBArGXQSugAA/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzipCveLpe-hDeprTmD-sH-s3zxG8Q1CgcpVbxI3yBXCE4fXDLLCwhC9bx6Age8Gyr5VQ/exec',
   SHEET_ID: '114sl6Mt-UhQQsv7zyicAAmsYzo3VDPoAvbT-0MakK94',
   GUEST_PERCENT: 0.10,
   CONTACT_EMAIL: 'contacto@sintropiasocial.com',
